df3159340599123a36fad5336e677d9fcbd3d543
Luis Sanchez <luis.sanchezz.vazquezz@gmail.com>
2023-11-01 19:50:07 -0600

    🆕 feat: Add interface for 'bundles' endpoint, version change to 1.6.4

--- src/interfaces/v1/endpoints/bundles.ts
+++ src/interfaces/v1/endpoints/bundles.ts
@@ -1,1 +1,13 @@
-export interface Bundles {}
+export interface Bundle {
+	uuid: string
+	displayName: string
+	displayNameSubText: null | string
+	description: string
+	extraDescription: null | string
+	promoDescription: null | string
+	useAdditionalContext: boolean
+	displayIcon: string
+	displayIcon2: string
+	verticalPromoImage: null | string
+	assetPath: string
+}
