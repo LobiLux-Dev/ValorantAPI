896903a18a1d902a89dcab9de64ce69df2c29697
Luis Sanchez <luis.sanchezz.vazquezz@gmail.com>
2023-11-01 19:47:11 -0600

    🆕 feat: Add interfaces for 'agents' endpoint, version change to 1.6.2

--- src/interfaces/v1/endpoints/agents.ts
+++ src/interfaces/v1/endpoints/agents.ts
@@ -1,1 +1,64 @@
-export interface Agents {}
+export interface Agent {
+	uuid: string
+	displayName: string
+	description: string
+	developerName: string
+	characterTags: string[] | null
+	displayIcon: string
+	displayIconSmall: string
+	bustPortrait: null | string
+	fullPortrait: null | string
+	fullPortraitV2: null | string
+	killfeedPortrait: string
+	background: null | string
+	backgroundGradientColors: string[]
+	assetPath: string
+	isFullPortraitRightFacing: boolean
+	isPlayableCharacter: boolean
+	isAvailableForTest: boolean
+	isBaseContent: boolean
+	role: Role | null
+	recruitmentData: RecruitmentData | null
+	abilities: Ability[]
+	voiceLine: null
+}
+
+interface Ability {
+	slot: Slot
+	displayName: string
+	description: string
+	displayIcon: null | string
+}
+
+enum Slot {
+	Ability1 = 'Ability1',
+	Ability2 = 'Ability2',
+	Grenade = 'Grenade',
+	Passive = 'Passive',
+	Ultimate = 'Ultimate',
+}
+
+interface RecruitmentData {
+	counterId: string
+	milestoneId: string
+	milestoneThreshold: number
+	useLevelVpCostOverride: boolean
+	levelVpCostOverride: number
+	startDate: Date
+	endDate: Date
+}
+
+interface Role {
+	uuid: string
+	displayName: DisplayName
+	description: string
+	displayIcon: string
+	assetPath: string
+}
+
+enum DisplayName {
+	Controller = 'Controller',
+	Duelist = 'Duelist',
+	Initiator = 'Initiator',
+	Sentinel = 'Sentinel',
+}
